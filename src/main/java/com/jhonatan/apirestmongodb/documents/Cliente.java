be87c7cec9a83fb53c0ca5f3e311f574de8f8b90
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-03-14 15:09:23 -0500

    document client

--- src/main/java/com/jhonatan/apirestmongodb/documents/Cliente.java
+++ src/main/java/com/jhonatan/apirestmongodb/documents/Cliente.java
@@ -1,4 +1,4 @@
-package com.jhonatan.documents;
+package com.jhonatan.apirestmongodb.documents;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
