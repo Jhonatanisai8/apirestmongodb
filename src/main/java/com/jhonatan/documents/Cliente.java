3647ca4d06c08c519e1486dbfaeb75214f832892
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-03-14 15:06:32 -0500

    document client

---
+++ src/main/java/com/jhonatan/documents/Cliente.java
@@ -0,0 +1,34 @@
+package com.jhonatan.documents;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document
+public class Cliente {
+
+    @Id
+    private String clienteId;
+
+    @NotEmpty
+    private String nombre;
+
+    @NotEmpty
+    private String apellido;
+
+    @NotNull
+    private Integer edad;
+
+    @NotNull
+    private Double sueldo;
+
+    private String foto;
+}
