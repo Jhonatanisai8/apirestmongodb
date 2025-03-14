567407dc2c519cf8a6e76ed84a13a4e5551b7613
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-03-14 14:54:58 -0500

    dependencia e first commmit

---
+++ src/main/java/com/jhonatan/apirestmongodb/ApirestmongodbApplication.java
@@ -0,0 +1,13 @@
+package com.jhonatan.apirestmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApirestmongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApirestmongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jhonatan/apirestmongodb/ApirestmongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhonatan.apirestmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApirestmongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
